a8c2f5eabe31d7d234abdafb54a5b35213b137b0
bartvanhoey <bartvanhoey@hotmail.com>
2018-09-17 22:48:27 +0200

    end clip 26 angular and ASP.NET Core Web API

---
+++ EmployeWebAPIService/Controllers/EmployeesController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeWebAPIService.Controllers
+{
+    [Route("api/employees")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly EmployeeContext _employeeContext;
+
+        public EmployeesController(EmployeeContext employeeContext)
+        {
+            _employeeContext = employeeContext;
+        }
+
+        [HttpGet]
+        public  IActionResult GetEmployees()
+        {
+           return Ok(_employeeContext.Employees.ToList());
+        }
+        
+        [HttpGet("{code}")]
+        public  IActionResult GetEmployee(string code)
+        {
+            return Ok(_employeeContext.Employees.Where(x => x.Code == code).ToList());
+        }
+        
+    }
+}
